10ffe5f6d0e8dc0162b5379f3d6abb42fb4a7821
cristibctr <cristian.bucataru@gmail.com>
2024-01-12 15:10:53 +0200

    Add a way to input data

--- SecretStudentVotingScheme/src/main/java/com/bctr/ssvs/Main.java
+++ SecretStudentVotingScheme/src/main/java/com/bctr/ssvs/Main.java
@@ -4,31 +4,36 @@ import com.bctr.ssvs.voting.Administrator;
 import com.bctr.ssvs.voting.Counter;
 import com.bctr.ssvs.voting.Vote;
 import com.bctr.ssvs.voting.Voter;
+import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static Counter counter = new Counter();
-    public static List<Voter> voters = new ArrayList<>(List.of(
-            new Voter(counter),
-            new Voter(counter),
-            new Voter(counter),
-            new Voter(counter),
-            new Voter(counter)
-    ));
-    public static Administrator administrator = Administrator.getAdministrator(voters);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Counter counter = new Counter();
+        Path path = Path.of("src/main/resources/votes.txt");
+        Map<Voter, Vote> voteMap = Files.newBufferedReader(path)
+                .lines()
+                .collect(Collectors.toMap(vote -> new Voter(counter), Vote::new));
+        Administrator administrator = Administrator.getAdministrator(List.copyOf(voteMap.keySet()));
         //An attacker trying to manipulate the voting process should not be allowed to vote
 //        Voter fakeVoter = new Voter(counter);
 //        fakeVoter.setAdministrator(administrator);
-//        voters.add(fakeVoter);
-        voters.forEach(voter -> {
-            voter.prepareBallot(new Vote(String.valueOf(ThreadLocalRandom.current().nextInt(1, 11))));
-            voter.sendBallot();
+//        voteMap.put(fakeVoter, new Vote("3"));
+        voteMap.forEach((key, value) -> {
+            key.prepareBallot(value);
+            key.sendBallot();
             administrator.publishSignedVotersList();
-            voter.sendVote();
+            key.sendVote();
             counter.publishBallots();
         });
         System.out.println(counter.countVotes());
